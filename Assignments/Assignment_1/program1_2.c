87d842bde6fdd8a0f5f8d5d487f0bc730be1b9ad
Gauribodke24 <gauribodke24@gmail.com>
2025-11-03 13:38:37 +0530

    Add Assignment1 Solution (Q1-Q5)

---
+++ Assignments/Assignment_1/program1_2.c
@@ -0,0 +1,25 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Display
+//  Description :   It is used to print 5 times "Marvellous" on screen
+//  Author :        Gauri Shekhar Bodke
+//  Date:           02/11/2025
+//
+/////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+
+void Display()
+{
+    int i=0;
+    for(i=1;i<=5;i++)
+    {
+        printf("Marvellous\n");
+    }
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
