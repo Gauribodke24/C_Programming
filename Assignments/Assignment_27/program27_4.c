cd1ce21118a2fc27a0b2e0f154c27af05c0f3be6
Gauribodke24 <gauribodke24@gmail.com>
2026-01-20 00:02:11 +0530

    Add Assignment_27 Solutions(Q1-Q5)

---
+++ Assignments/Assignment_27/program27_4.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+
+////////////////////////////////////////////////////////////////////
+//  Function :      Pattern
+//  Description :   It is used to display pattern in matrix form
+//  Input :         Integer, Integer
+//  Output :        void
+//  Author :        Gauri Shekhar Bodke
+//  Date :          18/12/2025
+//
+////////////////////////////////////////////////////////////////////
+
+/*
+    Enter number of row :
+    4
+    Enter number of column :
+    4
+
+    *       #       *       #
+    *       #       *       #
+    *       #       *       #
+    *       #       *       #
+    
+    
+*/
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0;
+    int j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(j % 2 == 0)
+            {
+                printf("#\t");
+            }
+            else
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of row :\n");
+    scanf("%d", &iValue1);
+
+    printf("Enter number of column :\n");
+    scanf("%d", &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
